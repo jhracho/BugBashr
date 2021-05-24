a0dad6592bbf8b899b26c45fcc21faff7a33f19d
jhracho <jhracho@nd.edu>
2021-05-24 17:50:38 -0400

    deleting projects deletes all associated bugs

--- scripts/projectManagement.php
+++ scripts/projectManagement.php
@@ -22,8 +22,8 @@ if (isset($_POST['project-submit-button'])){
         echo"<script>alert(\"A project with that name already exists. Please rename your project.\")</script>";
     }
     else{
-        $add_project = mysqli_prepare($con, "INSERT INTO projects VALUES (NULL, ?, ?, ?, CURDATE(), ?, 0)");
-        mysqli_stmt_bind_param($add_project, "isss", $user_id, $projectTitle, $projDescription, $projDeadline);
+        $add_project = mysqli_prepare($con, "INSERT INTO projects VALUES (NULL, ?, ?, ?, CURDATE(), ?, 0, ?)");
+        mysqli_stmt_bind_param($add_project, "issss", $user_id, $projectTitle, $projDescription, $projDeadline, $user_id);
         mysqli_stmt_execute($add_project);
         mysqli_stmt_close($add_project);        
     }
