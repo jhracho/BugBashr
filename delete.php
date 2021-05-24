43c0e7296a16004c2b0e6daed19681d8376ab644
jhracho <jhracho@nd.edu>
2021-05-24 17:50:23 -0400

    people without project access can't delete bugs

--- delete.php
+++ delete.php
@@ -3,12 +3,32 @@
 
 session_start();
 $username     = $_SESSION['username'];
-$projectTable = $_SESSION['projectTable'];
-$project      = $_SESSION['project'];
+$user_id      = $_SESSION['user_id'];
+$has_access   = True;
 
 if (isset($_GET['ID'])){
-    $bugID = $_GET['ID'];
-    
+    // Get the users assigned to the project
+    $bug_id = $_GET['ID'];
+    $check_access = mysqli_prepare($con, "SELECT has_access FROM bugs, projects WHERE bugs.project_id = projects.project_id AND bug_id = ?");
+    mysqli_stmt_bind_param($check_access, "i", $bug_id);
+    mysqli_stmt_execute($check_access);
+    mysqli_stmt_bind_result($check_access, $has_access);
+    mysqli_stmt_fetch($check_access);
+    mysqli_stmt_close($check_access);
+
+    // Check if the user has access to the project
+    $found = strpos($has_access, $bug_id);
+
+    // Interpret the query result
+    if ($found !== false){
+        $delete_bug = mysqli_prepare($con, 'DELETE FROM bugs WHERE bug_id = ?');
+        mysqli_stmt_bind_param($delete_bug, "i", $bug_id);
+        mysqli_stmt_execute($delete_bug);
+        mysqli_stmt_close($delete_bug);
+    }
+    else{
+        echo "Cannot access this page";
+    }
 }
 else{
     echo "Cannot access this page";
