31ee2d40ea54e1da64acdb2d79916ee6f146a438
jhracho <jhracho@nd.edu>
2021-05-26 17:47:04 -0400

    added PM to project modals

--- scripts/populateHome.php
+++ scripts/populateHome.php
@@ -2,10 +2,11 @@
 <?php
 // DYNAMICALLY ADD PROJECT MODALS
     $user_id = $_SESSION['user_id'];
-    $get_projects = mysqli_prepare($con, "SELECT project_id, project_name, project_desc, creation_date, total_bugs FROM projects WHERE user_id = ?");
-    mysqli_stmt_bind_param($get_projects, "i", $user_id);
+    $get_projects = mysqli_prepare($con, "SELECT projects.user_id, first_name, last_name, project_id, project_name, project_desc, creation_date, total_bugs FROM users, projects WHERE projects.user_id = users.user_id AND projects.has_access LIKE ?;");
+    $helper = '%'.$user_id.'%';
+    mysqli_stmt_bind_param($get_projects, "s", $helper);
     mysqli_stmt_execute($get_projects);
-    mysqli_stmt_bind_result($get_projects, $project_id, $project_name, $project_desc, $created_at, $total_bugs);
+    mysqli_stmt_bind_result($get_projects, $project_user_id, $first_name, $last_name, $project_id, $project_name, $project_desc, $created_at, $total_bugs);
     while(mysqli_stmt_fetch($get_projects)){
         $modal_name = "".$project_id."-modal";
         echo"<div class='modal fade' tabindex='-1' role='dialog' id='".$modal_name."'>
@@ -18,14 +19,21 @@
                 <div class='modal-body'>
                     <p><strong>Description:</strong></p>
                     <p>".$project_desc."</p>
+                    <p><strong>Project Manager:</strong></p>
+                    <p>{$first_name} {$last_name}</p>
                     <p><strong>Date Added:</strong></p>
                     <p>".$created_at."</p>
                     <p><strong>Tracked Bugs: </strong>".$total_bugs."</p>
                 </div>
                 <div class='modal-footer'>
-                    <a class='btn btn-primary' href='details?ID=".$project_id."'>View/Manage Bugs</a>
-                    <a class='btn btn-primary' href='team?ID=".$project_id."'>Manage Team</a>
-                </div>
+                    <a class='btn btn-primary' href='details?ID=".$project_id."'>View/Manage Bugs</a>";
+                    if ($user_id === $project_user_id){
+                        echo "<a class='btn btn-primary' href='teamAuth?ID=".$project_id."'>Manage Team</a>";
+                    }
+                    else{
+                        echo "<a class='btn btn-primary' href='teamAuth?ID=".$project_id."'>View Team</a>";
+                    }
+                echo "</div>
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
         </div><!-- /.modal -->";
